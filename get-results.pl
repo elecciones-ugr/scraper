9355f4c94b168738d49c0fd8c7f9d8e3373a6f30
JJ Merelo <jjmerelo@gmail.com>
2015-05-23 18:13:34 +0200

    Saca también CSV

--- get-results.pl
+++ get-results.pl
@@ -20,6 +20,8 @@ my $candidaturas = $dom->find("table.unnamed1");
 
 my %results = ( Escrutado => $porcentaje,
 		Resultados => {} );
+
+my @results_csv = (["Candidato", "Sector","Coeficiente", "Votos","Resultado"]);
 for my $c (@$candidaturas ) {
   my $rows = $c->find("table tr");
   my $who_ref = shift @$rows ;
@@ -35,17 +37,21 @@ for my $c (@$candidaturas ) {
     my @these_cols = @$cols;
     my $res = $r->find('td')->map('text');
     my $results_sector = {};
+    my @column_values = ();
     while ( my $l = shift @these_cols ) {
 	my $this_result = shift @$res;
 	$this_result =~ s/,/./g;
 	$results_sector->{$l} = $this_result;
+	push @column_values, $this_result;
     }
     push @{$results{'Resultados'}{$who}{'Sector'}}, $results_sector;
+    push @results_csv, [$who, @column_values];
   }
 }
 
 my $output = encode_json \%results;
 
+write_file("$output_fn.csv", join("\n",map( join("; ", @$_), @results_csv)));
 write_file("$output_fn.json",$output);
 my $jsonp = "parse_results( $output )";
 write_file("$output_fn.jsonp",$jsonp);
